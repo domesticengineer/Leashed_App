57ec4ab0f93525b4d07ba0341875df9385e486b5
domesticengineer <domesticengineer83@gmail.com>
2025-07-25 04:28:55 -0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('addButton').addEventListener('click', function() {
-  let taskText = document.getElementById('taskInput').value;
-  if (taskText === '') return;
-  let newLi = document.createElement('li');
-  newLi.textContent = taskText;
-  document.getElementById('taskList').appendChild(newLi);
-  document.getElementById('taskInput').value = '';
-});
